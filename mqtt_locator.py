ce7077fae5a0010630111728eaffedcacc6de3c3
Austen Higgins-Cassidy <austen@thelevelup.com>
2017-10-06 11:36:17 -0400

    PIP8 Cleanup

--- mqtt_locator.py
+++ mqtt_locator.py
@@ -2,17 +2,15 @@
 
 from __future__ import print_function
 
-import pygame
-from pygame.locals import *
-import json
-import logging
-import paho.mqtt.client as mqtt
-from pprint import pprint
-from uszipcode import ZipcodeSearchEngine
-import random
-import time
 import ConfigParser
+import json
+import random
 import sys
+import time
+
+import paho.mqtt.client as mqtt
+import pygame
+from uszipcode import ZipcodeSearchEngine
 
 class Ping(object):
     """A ping on the map"""
@@ -22,35 +20,35 @@ class Ping(object):
         (0x1E, 0xBB, 0xF3),
         (0x71, 0xCB, 0x3A)]
 
-    def __init__(self, x, y):
+    def __init__(self, x_loc, y_loc):
         self.created_time = time.time()
         self.life_time = 1
         self.color = random.choice(Ping.colors)
         self.size = 20
         self.grow_limit = 5
-        self.x = x
-        self.y = y
+        self.coordinate = [x_loc, y_loc]
 
-    def isAlive(self):
+    def is_alive(self):
+        """Returns true if we are within lifetime, false otherwise"""
         return (time.time() - self.created_time) < self.life_time
 
-    def lifeFactor(self):
+    def life_factor(self):
+        """Gets a scaling factor based on life remaining"""
         return (time.time() - self.created_time) / self.life_time
 
     def draw(self, win):
-        radius = int(round(self.lifeFactor() * self.size))
-        if (radius < self.grow_limit):
+        """Renders a ping to a display window"""
+        radius = int(round(self.life_factor() * self.size))
+        if radius < self.grow_limit:
             thickness = radius
         else:
             thickness = self.grow_limit
 
-        pygame.draw.circle(win, self.color, [self.x, self.y], radius, thickness)
-
-def on_connect(client, userdata, flags, rc):
-    client.subscribe(self)
+        pygame.draw.circle(win, self.color, self.coordinate, radius, thickness)
 
 class Map(object):
-    def __init__(self,mqtt_info):
+    """A class to render the map and pings"""
+    def __init__(self, mqtt_info):
         pygame.display.init()
         self.pings = []
         self.current_time = time.time()
@@ -59,55 +57,60 @@ class Map(object):
         self.client = mqtt.Client()
         self.client.on_connect = lambda c, d, f, rc: self.on_connect(c,d,f,rc)
         self.client.on_message = lambda c, d, m: self.on_message(c,d,m)
-        self.client.connect(self.mqtt_info["host"], int(self.mqtt_info["port"]), int(self.mqtt_info["keepalive"]))
+        self.client.connect(self.mqtt_info["host"],
+                            int(self.mqtt_info["port"]),
+                            int(self.mqtt_info["keepalive"]))
         self.background = pygame.image.load("map.PNG")
         self.x_shift = self.background.get_width() / 2.0
         self.y_shift = self.background.get_height() / 2.0
         self.x_scale = self.x_shift / 180.0
         self.y_scale = self.y_shift /  90.0
         self.screen_size = [self.background.get_width(), self.background.get_height()]
-        logging.warning("Scale (" + str(self.x_scale) + "," + str(self.y_scale) + ")")
-        logging.warning("Shift (" + str(self.x_shift) + "," + str(self.y_shift) + ")")
         self.zips = None
         self.win = pygame.display.set_mode(self.screen_size, pygame.NOFRAME)
         self.client.loop_start()
 
-    def on_connect(self, client, flags, userdata, rc):
+    def on_connect(self, client, _flags, userdata, rc):
+        """MQTT Connection callback"""
+        print("Connected with result code {}".format(rc))
+        print()
         client.subscribe(self.mqtt_info["topic"])
 
-    def on_message(self, client, userdata, message):
+    def on_message(self, _client, userdata, message):
+        """MQTT Message recieved callback"""
         payload = json.loads(message.payload)
-        if payload["postal_code"] == None or payload["postal_code"] == "":
+        if payload["postal_code"] is None or payload["postal_code"] == "":
             return
-        if (self.zips == None):
+        if self.zips is None:
             self.zips = ZipcodeSearchEngine()
         zcode = self.zips.by_zipcode(payload["postal_code"])
-        if zcode["Longitude"] == None or zcode["Latitude"] == None:
+        if zcode["Longitude"] is None or zcode["Latitude"] is None:
             return
-        (x,y) = self.project(zcode["Longitude"], zcode["Latitude"])
-        x = int(x)
-        y = int(y)
-        logging.warning(zcode["City"] + " at " + str(x) + ", " + str(y))
-        self.pings.append(Ping(x,y))
+        (x_coord, y_coord) = self.project(zcode["Longitude"], zcode["Latitude"])
+        self.pings.append(Ping(x_coord, y_coord))
 
     def draw(self):
-        self.win.fill((59,175,218))
-        self.win.blit(self.background,(0,0))
+        """Render the map and it's pings"""
+        self.win.fill((59, 175, 218))
+        self.win.blit(self.background, (0, 0))
         for ping in self.pings[:]:
-            if ping.isAlive():
+            if ping.is_alive():
                 ping.draw(self.win)
             else:
                 self.pings.remove(ping)
 
     def project(self, lon, lat):
-        x = (self.x_scale * lon) + self.x_shift
-        y = self.y_shift - (self.y_scale * lat)
-        return (x, y)
+        """Convert lat/long to pixel x/y"""
+        x_coord = (self.x_scale * lon) + self.x_shift
+        y_coord = self.y_shift - (self.y_scale * lat)
+        return (int(x_coord), int(y_coord))
 
     def quit(self):
+        """Cleanup"""
         self.client.loop_stop()
 
 def read_config(config_file):
+    """Global function to read external config file"""
     config = ConfigParser.SafeConfigParser()
     read = config.read(config_file)
     if not read:
@@ -117,6 +120,7 @@ def read_config(config_file):
     return dict(config.items('map'))
 
 def main():
+    """Script Entry Point"""
     if len(sys.argv) != 2:
         print("Usage: {} CONFIG_FILE".format(sys.argv[0]))
         print()
