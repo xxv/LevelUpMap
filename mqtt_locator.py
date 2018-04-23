368d712b2d82b1e52aaa7c12737d7b09993cae5e
Steve Pomeroy <stevep@thelevelup.com>
2018-04-23 18:40:38 -0400

    Add profiler in

--- mqtt_locator.py
+++ mqtt_locator.py
@@ -219,5 +219,18 @@ def main():
     pygame.quit()
 
 
+def main_profiled():
+    import cProfile, pstats, StringIO
+    pr = cProfile.Profile()
+    pr.enable()
+    main()
+    pr.disable()
+    s = StringIO.StringIO()
+    sortby = 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
+
+
 if __name__ == '__main__':
     main()
