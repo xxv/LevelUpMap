9d059d7240a2e60b8e713a3054fcb873ef50398c
Steve Pomeroy <stevep@thelevelup.com>
2018-05-07 16:17:17 -0400

    Improve heatmap dirty handling

--- heatmap.py
+++ heatmap.py
@@ -28,13 +28,13 @@ class Heatmap(object):
         scaled_pos = (int(position[0]/self._scale_factor),
                       int(position[1]/self._scale_factor))
         self._data[scaled_pos] += 1
-        now = datetime.now()
-        if not self._last_update or (now > (self._last_update + self._update_frequency)):
-            self._dirty = True
-            self._last_update = now
+        self._dirty = True
 
     def render(self):
-        if self._dirty:
+        now = datetime.now()
+        if self._dirty and (
+                not self._last_update or (
+                    now > (self._last_update + self._update_frequency))):
             d_max = self._data.max()
             if d_max > 0:
                 self._norm = np.log(self._data + np.ones(self._data.shape))
@@ -45,13 +45,14 @@ class Heatmap(object):
             pixel_data = pygame.surfarray.pixels_alpha(self._boxes)
             pixel_data[:] = self._norm
             pygame.transform.scale(self._boxes, self._scaled_surface.get_size(), self._scaled_surface)
-            self._dirty = False
             if self._bg_surface:
                 self._bg_surface.fill(self._bgcolor)
                 self._bg_surface.blit(self._scaled_surface, (0, 0))
                 self._surface = self._bg_surface
             else:
                 self._surface = self._scaled_surface
+            self._dirty = False
+            self._last_update = now
 
         return self._surface
 
